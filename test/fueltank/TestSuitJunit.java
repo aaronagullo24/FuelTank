8f5edfefd4836b2a4cbdab0a81ada2c85dee55cd
Aaron Agullo <Aaronagullo24@gmail.com>
2019-05-02 21:30:39 +0200

    ultimos retoques

---
+++ test/fueltank/TestSuitJunit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fueltank;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ *
+ * @author Aaron
+ */
+
+@RunWith(Suite.class)
+@SuiteClasses({FuelTankTest.class,ParametizadaConsume.class,ParametrizadaFill.class})
+public class TestSuitJunit {
+    
+}
